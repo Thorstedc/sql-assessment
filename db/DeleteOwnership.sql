cd8709e0d9814e8c6fe4d96e5a9ad5edea4e2339
Christopher <thorstedc266935@gmail.com>
2017-12-06 22:37:46 -0700

    finished

---
+++ db/DeleteOwnership.sql
@@ -0,0 +1,3 @@
+UPDATE vehicles
+SET owner_id = null WHERE owner_id = $2 AND id = $1
+RETURNING *;
--- server.js
+++ server.js
@@ -13,19 +13,19 @@ app.use(cors());
 // You need to complete the information below to connect
 // to the assessbox database on your postgres server.
 massive({
-  host: //host,
-  port: //port,
-  database: //database,
-  user: //user,
-  password: //password
+  host: 'localhost', //host,
+  port: 5432,//port,
+  database: 'assessbox',//database,
+  user: 'postgres',//user,
+  password: 'password'//password
 }).then( db => {
   app.set('db', db);
 
   // Initialize user table and vehicle table.
   db.init_tables.user_create_seed().then( response => {
-    console.log('User table init');
+    console.log('hello from User table');
     db.init_tables.vehicle_create_seed().then( response => {
-      console.log('Vehicle table init');
+      console.log('hello from Vehicle table');
     })
   })
 
@@ -34,6 +34,20 @@ massive({
 
 // ===== Build enpoints below ============
 
+app.get('/api/users', mainCtrl.getAllUsers);
+app.get('/api/vehicles', mainCtrl.getAllVehicles);
+app.get('/api/user/:id/vehiclecount', mainCtrl.getCountCarsByID);
+app.get('/api/user/:id/vehicle', mainCtrl.getCarsByUserID);
+app.get('/api/newervehiclesbyyear', mainCtrl.getVehiclesByYear);
+app.get('/api/vehicle', mainCtrl.getQuery);
+
+app.put('/api/vehicle/:vid/user/:uid', mainCtrl.UpdateVehicleOwner);
+
+app.post('/api/users', mainCtrl.addUser);
+app.post('/api/vehicles', mainCtrl.addVehicle);
+
+app.delete('/api/user/:uid/vehicle/:vid', mainCtrl.DeleteOwnership);
+app.delete('/api/vehicle/:id', mainCtrl.DeleteVehicleByID);
 
 
 
